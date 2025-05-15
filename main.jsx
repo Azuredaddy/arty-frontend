c6aded0a6c4ff25efbd79fcef1e6dbf6db3704b0
Azuredaddy <141799941+Azuredaddy@users.noreply.github.com>
2025-05-15 12:37:24 +1000

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-const App = () => <h1>Arty Portal is Live 🎉</h1>
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
