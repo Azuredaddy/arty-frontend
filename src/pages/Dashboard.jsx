0d065cf6a4e969afd6c0a3069ad55ae56e45e134
Azuredaddy <141799941+Azuredaddy@users.noreply.github.com>
2025-05-16 12:52:38 +1000

    Delete src directory

--- src/pages/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
-      <p>Welcome to Arty’s working dashboard page. 🎉</p>
-    </div>
-  )
-}
