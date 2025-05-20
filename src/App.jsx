2bf1ec07e392d3d9e67a9fe95f1acb208dbc0d6e
Azuredaddy <141799941+Azuredaddy@users.noreply.github.com>
2025-05-20 16:24:18 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
+
 export default App;
