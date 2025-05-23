b692d643c60ab16b2776cd0090b1a397b830633f
Azuredaddy <141799941+Azuredaddy@users.noreply.github.com>
2025-05-23 15:59:55 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./Layout";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./pages/ProtectedRoute";
 import { AuthProvider } from "./contexts/AuthContext";  // Import AuthProvider
 
 function AppRoutes() {
