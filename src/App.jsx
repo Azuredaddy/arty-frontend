b4c2b5e61c2e7d5cff56056a8d7fa33b792528cc
Azuredaddy <141799941+Azuredaddy@users.noreply.github.com>
2025-05-27 16:33:27 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import TenantGate from './components/TenantGate';
 
 const App = () => {
   return (
-    <Router>
-      <TenantGate>
-        <Routes>
-          <Route path="/*" element={<Layout />} />
-        </Routes>
-      </TenantGate>
-    </Router>
+    <TenantGate>
+      <Routes>
+        <Route path="/*" element={<Layout />} />
+      </Routes>
+    </TenantGate>
   );
 };
 
